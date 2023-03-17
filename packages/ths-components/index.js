dba617cf8db315c2c52ad6bf6c4b2bdcc51cfeac
lichun <zheyuan54@qq.com>
2023-03-17 18:18:33 +0800

    feat: 测试 pnpm 关联本地项目包依赖

---
+++ packages/ths-components/index.js
@@ -0,0 +1,5 @@
+import { testFunc, testFunc222 } from "@ths-design/themes";
+
+testFunc(222)
+
+testFunc222(333)
---
+++ packages/ths-themes/index.js
@@ -0,0 +1,9 @@
+export const a = 'test111222'
+
+export const testFunc = (a) => {
+  console.log(a)
+};
+
+export const testFunc222 = (a) => {
+  console.log(a)
+};
